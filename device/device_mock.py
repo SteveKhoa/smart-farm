15ca4942f7a0ec4e88cec75a6e3491e1eb4ed69a
Khoa LTN <vietnamkhoale@gmail.com>
2025-05-31 15:38:34 +0700

    device

---
+++ device/device_mock.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import random
+import json
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+
+from payload import soil_moisture
+from device import config, mqtt
+
+import paho.mqtt.client as mqttclient
+import time
+
+
+def main():
+    device_configs = [
+        (mqttclient.Client(os.getenv("TEMPERATURE_CLIENT_ID")), "temperature"),
+        (mqttclient.Client(os.getenv("HUMIDITY_CLIENT_ID")), "humidity"),
+        (mqttclient.Client(os.getenv("MOISTURE_CLIENT_ID")), "moisture"),
+        (mqttclient.Client(os.getenv("LIGHT_CLIENT_ID")), "light"),
+    ]
+
+    for conf in device_configs:
+        device, data_key = conf
+
+        device.on_connect = mqtt.connected
+        device.on_subscribe = mqtt.subscribed
+        device.on_message = mqtt.recv_message
+        device.connect(config.BROKER_ADDRESS, config.BROKER_PORT)
+        device.loop_start()
+
+        device.username_pw_set(
+            os.getenv("COMMON_USERNAME"), os.getenv("COMMON_PASSWORD")
+        )
+
+        # Custom attributes
+        device.data_key = data_key
+
+    while True:
+        value = random.randint(0, 100)
+
+        for conf in device_configs:
+            client, _ = conf
+            client.publish(
+                "v1/devices/me/telemetry",
+                json.dumps(
+                    {
+                        f"value": value,
+                    }
+                ),
+                1,
+            )
+
+        time.sleep(1)
+        print("send value:", value)
+
+
+if __name__ == "__main__":
+    main()
